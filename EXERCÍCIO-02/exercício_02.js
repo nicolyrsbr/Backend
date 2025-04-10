d26121ca98b856f198f86643ece366720c81791b
nicolyrsbr <nicolyrsbr@gmail.com>
2025-04-10 11:40:19 -0400

    Exercicios_02 - aula 3

---
+++ EXERCÍCIO-02/exercício_02.js
@@ -0,0 +1,5 @@
+let julia = 50;
+let marcos = 20;
+
+let msg = julia > marcos ? "Julia é mais velha que Marcos" : "Marcos é mais velho que Julia";
+console.log(msg);
---
+++ EXERCÍCIO-02/exercício_04.js
@@ -0,0 +1,6 @@
+numero = 10;
+let verificarSeEPar = numero % 2 == 0;
+
+let msg = verificarSeEPar == 0 ? "Par" : "Impar";
+
+console.log(msg);
