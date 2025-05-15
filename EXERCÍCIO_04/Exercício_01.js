80f5633070e5b445149372f6e9218fc8b72fa9a0
nicolyrsbr <nicolyrsbr@gmail.com>
2025-05-15 11:45:35 -0400

    começando exercicios estruturas condicionais

---
+++ EXERCÍCIO_04/Exercício_01.js
@@ -0,0 +1,30 @@
+import entradaDados from 'readline-sync';
+
+let dia = entradaDados.question('Informe o dia da semana(1-7): ');
+
+switch (dia) {
+    case 1:
+        console.log('Domingo');
+        break
+    case 2:
+        console.log('Segunda');
+        break
+    case 3:
+        console.log('Terça');
+        break
+    case 4:
+        console.log('Quarta');
+        break
+    case 5:
+        console.log('Quinta');
+        break
+    case 6:
+        console.log('Sexta');
+        break
+    case 7:
+        console.log('Sabado');
+        break
+    default:
+        console.log('Dia inválido');
+        break
+}
---
+++ EXERCÍCIO_04/Exercício_02.js
@@ -0,0 +1,31 @@
+
+import entradaDados from 'readline-sync';
+
+let mes = entradaDados.question('Informe o dia da semana(1-7): ');
+
+switch (mes) {
+    case 1:
+        console.log('Domingo')
+    case 2:
+        console.log('Segunda')
+    case 3:
+        console.log('Terça')
+    case 4:
+        console.log('Quarta')
+    case 5:
+        console.log('Quinta')
+    case 6:
+        console.log('Sexta')
+    case 7:
+        console.log('Sabado')
+    case 8:
+        console.log('Segunda')
+    case 9:
+        console.log('Terça')
+    case 10:
+        console.log('Quarta')
+    case 11:
+        console.log('Quinta')
+    case 12:
+        console.log('Sexta')
+}
